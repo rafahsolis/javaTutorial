df3a57c722f160c85906e0895037129cc507cc7b
Rafael Herrero <rafahsolis@gmail.com>
2015-04-02 14:25:40 +0200

    package tutorial1.ocean

--- packageexample.java
+++ packageexample.java
@@ -1,7 +1,9 @@
 package tutorial1;
 
 import tutorial1.ocean.Fish;
+import tutorial1.ocean.Seaweed;
 
 public class packageexample {
 	Fish fish1 = new Fish();
+	Seaweed seaweed1 = new Seaweed();
 }
