8503ff6f1faefeedca3938953c4ba3556c77b952
Rafael Herrero <rafahsolis@gmail.com>
2015-04-04 09:02:41 +0200

    Enum type

--- EnumTypes.java
+++ EnumTypes.java
@@ -3,8 +3,36 @@ package tutorialJava;
 public class EnumTypes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Animal animal = Animal.CAT;
+		
+		switch(animal){
+		case CAT:
+			System.out.println("Cat");
+			break;
+		case DOG:
+			System.out.println("Dog");
+			break;
+		case MOUSE:
+			System.out.println("Mouse");
+			break;
+		default:
+			break;
+		
+		}
 
+		System.out.println("Enum name as a string: " + Animal.DOG.name());
+		
+		System.out.println(Animal.DOG);
+		
+		System.out.println(Animal.DOG.getClass());
+		
+		System.out.println(Animal.DOG instanceof Enum);
+		
+		System.out.println(Animal.MOUSE.getName());
+		
+		Animal animal2 = Animal.valueOf("CAT");
+		
+		System.out.println(animal2);
 	}
 
 }
